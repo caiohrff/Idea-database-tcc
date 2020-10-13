7fee8c5df9005851173d9cd2cc682dd27afb5af1
Caio Henrique <caiohr2020@gmail.com>
2020-10-13 11:45:28 -0300

    Finsh RH | 13/10/2020

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = express()
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks')
+const { json } = require('body-parser')
 
 server.use(bodyParser.json())
 
@@ -76,16 +77,21 @@ server.post("/principal", (req, res) =>{
     const user = req.body.nome
     const pass = req.body.senha
 
-    
     mysqlConnection.query('SELECT EmpUser, EmpDepartament FROM employee WHERE EmpUser = "' + user +'" AND EmpPassword = "' + pass +'"',  (err, rows, field) =>{
-       rows[0].EmpDepartament
-
         if(!err){
             if(rows.length > 0){
                         if(rows[0].EmpDepartament === 'Tecnologia' || rows[0].EmpDepartament === 'Gestor da inovacao'){
-                             res.sendFile(__dirname + "/src/principalGestor.html")
+                            mysqlConnection.query('SELECT EmpName FROM employee WHERE EmpUser =  "' + user,  (err, rows, field) =>{
+                            console.log(rows)
+                            //     console.log(rows)
+                            // const usuario = JSON.stringify(rows)
+                            // const json = JSON.parse(resultado)
+                            // console.log(json)
+                            return res.render('principalGestor')
+                        })
+
                         }else{
-                             res.sendFile(__dirname + "/src/principalFunc.html")
+                            return res.render('principalFunc')
                         }
             }else{
                 res.sendFile(__dirname + "/src/index2.html")
@@ -106,7 +112,7 @@ server.post("/resultadoformulario", (req, res) =>{
         FormEmployeeCargo: req.body.cargo,
         FormAssunto: req.body.assunto
     }
-        mysqlConnection.query('INSERT INTO ideasform SET ?', DadosForm,(err, rows) => {
+        mysqlConnection.query('INSERT INTO ideasform SET ?', DadosForm, (err, rows) => {
             if(!err){
                 console.log('INSERIDO')
                 //SWEETALERT
@@ -129,15 +135,15 @@ server.post("/resultadoformulario", (req, res) =>{
             const dadosRH = {
                 employeeCPF: req.body.cpf,
                 employeeName: req.body.colaborador,
-                academicTraining: req.body.funcaoExercicio,
-                formacao: req.body.formacao,
+                currentJob: req.body.funcaoExercicio,
+                academicTraining: req.body.formacao,
                 admissionDate: req.body.DataAdmissao,
                 resignationDate: req.body.DataDemissao,
                 dedication: req.body.dedicacao,
                 annualCost: req.body.custoAnual,
                 hoursCost: req.body.custoHora,
                 pdHours: req.body.horasPD,
-                pdCost: req.body.custoPD, 
+                pdCost: req.body.custoPD,
            }
 
            //desestruturando o objeto principal em dois POIS AGORA NOSSA MANIPULAÇÃO É FEITA A PARTIR DOS OBJETOS: dadosRH1 | dadosRH2
@@ -148,30 +154,56 @@ server.post("/resultadoformulario", (req, res) =>{
                 }            
             })
 
-        let custoPD1 = +(dadosRH1.pdCost.replace(/,/,'.'))
-        let custoPD2= +(dadosRH2.pdCost.replace(/,/,'.'))  
-        let totalPdCost = custoPD1 + custoPD2 //PERSISTIR NO BANCO
+            let custoPD1 = +(dadosRH1.pdCost.replace(/,/,'.'))
+            let custoPD2= +(dadosRH2.pdCost.replace(/,/,'.'))  
+            let totalPdCost = custoPD1 + custoPD2 //PERSISTIR NO BANCO
 
-        let custoHors1 = +(dadosRH1.hoursCost.replace(/,/,'.'))
-        let custoHors2 = +(dadosRH2.hoursCost.replace(/,/,'.')) 
-        let totalPdHours = custoHors1 + custoHors2 //PERSISTIR NO BANCO
-        
-        let custoAno1 = +(dadosRH1.annualCost.replace(/,/,'.'))
-        let custoAno2 = +(dadosRH2.annualCost.replace(/,/,'.')) 
-        let totalAnualCost = custoAno1 + custoAno2 //PERSISTIR NO BANCO
-        
-        let totalHoraPD1 = +(dadosRH1.pdHours.replace(/,/,'.'))
-        let totalHoraPD2 = +(dadosRH2.pdHours.replace(/,/,'.'))
-        let totalHours = totalHoraPD1 + totalHoraPD2 //PERSISTIR NO BANCO
-        
-        console.log(totalPdCost, totalPdHours, totalAnualCost, totalHours)
+            let custoHors1 = +(dadosRH1.hoursCost.replace(/,/,'.'))
+            let custoHors2 = +(dadosRH2.hoursCost.replace(/,/,'.')) 
+            let totalPdHours = custoHors1 + custoHors2 //PERSISTIR NO BANCO
+            
+            let custoAno1 = +(dadosRH1.annualCost.replace(/,/,'.'))
+            let custoAno2 = +(dadosRH2.annualCost.replace(/,/,'.')) 
+            let totalAnualCost = custoAno1 + custoAno2 //PERSISTIR NO BANCO
+            
+            let totalHoraPD1 = +(dadosRH1.pdHours.replace(/,/,'.'))
+            let totalHoraPD2 = +(dadosRH2.pdHours.replace(/,/,'.'))
+            let totalHours = totalHoraPD1 + totalHoraPD2 //PERSISTIR NO BANCO
 
-         //a inserção será feita apartir das chaves e valores, não do objeto inteiro       
+            dadosRH1.totalPdCost = totalPdCost
+            dadosRH1.totalPdHours = totalPdHours
+            dadosRH1.totalAnualCost = totalAnualCost
+            dadosRH1.totalHours = totalHours
+            //-----------------------------------------------------------------
+            dadosRH2.totalPdCost = totalPdCost
+            dadosRH2.totalPdHours = totalPdHours
+            dadosRH2.totalAnualCost = totalAnualCost
+            dadosRH2.totalHours = totalHours
+
+            console.log(dadosRH2)
+
+        //a inserção será feita apartir das chaves e valores, não do objeto inteiro
+                 
+        //O PROBLEMA É A QUANTIDADE DE ARGUMENTOS PASSADOS NO INSERT
+        mysqlConnection.query('INSERT INTO rh SET ?', dadosRH1, (err, rows) => {
+            if(!err){
+                console.log("INSERIDO DADOS 01")
+                mysqlConnection.query('INSERT INTO rh SET ?', dadosRH2, (err, rows) => {
+                    if(!err){
+                        console.log("INSERIDO DADOS 02")
+                    }else{
+                        console.log(err)
+                    }
+                  })
+            }else{
+                console.log(err)
+            }
+          })
     })
 
 // END ROUTS
 
 
 server.listen(3000, ()=>{
-    console.log("iniciado!!")
+    console.log("iniciado na porta 3000")
 })
